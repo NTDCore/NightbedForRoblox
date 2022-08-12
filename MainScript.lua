babcfd54d905a37468d73c5153486ca5aaee3041
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-08-12 08:45:21 +0700

    update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,1 +1,31 @@
-
+repeat task.wait() until game:IsLoaded()
+if isfolder(customdir:gsub("/", "")) == false then
+	makefolder(customdir:gsub("/", ""))
+end
+if isfolder("Nightbed") == false then
+	makefolder("Nightbed")
+end
+if isfolder(customdir.."CustomModules") == false then
+	makefolder(customdir.."CustomModules")
+end
+if isfolder(customdir.."Profiles") == false then
+	makefolder(customdir.."Profiles")
+end
+if isfolder("Nightbed/assets") == false then
+	makefolder("Nightbed/assets")
+end
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/MainScript.lua'))()")
+    end
+end)
+if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/bedwars.lua"))()
+elseif game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua"))()
+elseif isfile("Nightbed/CustomModules/"..game.PlaceId..".lua") then
+    loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
+else    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/AnyGame.lua"))()
+end
