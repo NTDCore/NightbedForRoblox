0102cf548190b810c9f3fef39b8f638dcaee8bee
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-09-03 14:41:57 +0700

    update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2,80 +2,82 @@ repeat task.wait() until game:IsLoaded()
 if not isfile("NightbedConfig.JSON") then writefile("NightbedConfig.JSON","{}")
 end
 local betterisfile = function(file)
-	local suc, res = pcall(function() return readfile(file) end)
-	return suc and res ~= nil
+local suc, res = pcall(function() return readfile(file) end)
+return suc and res ~= nil
 end
 local function GetURL(scripturl)
-	if shared.NightbedDeveloper then
-		if not betterisfile("Nightbed/"..scripturl) then
-			error("File not found : Nightbed/"..scripturl)
-		end
-		return readfile("Nightbed/"..scripturl)
-	else
-		local res = game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/"..scripturl, true)
-		assert(res ~= "404: Not Found", "File not found")
-		return res
-	end
+if shared.NightbedDeveloper then
+if not betterisfile("Nightbed/"..scripturl) then
+error("File not found : Nightbed/"..scripturl)
+end
+return readfile("Nightbed/"..scripturl)
+else
+	local res = game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/"..scripturl, true)
+assert(res ~= "404: Not Found", "File not found")
+return res
+end
 end
 local getasset = getsynasset or getshitmotherasset or getcustomasset or function(location) return "rbxasset://"..location end
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/MainScript.lua'))()")
-    end
-end)
+	if State == Enum.TeleportState.Started then
+	queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/MainScript.lua'))()")
+	end
+	end)
 local kavo = loadstring(GetURL("Libraries/kavo.lua"))()
-local window = kavo.CreateLib("Nightbed V2.2.1d", "Luna")
+local window = kavo.CreateLib("Nightbed", "Luna")
 shared.kavo = kavo
 local function getcustomassetfunc(path)
-	if not betterisfile(path) then
-		spawn(function()
-			local textlabel = Instance.new("TextLabel")
-			textlabel.Size = UDim2.new(1, 0, 0, 36)
-			textlabel.Text = "Downloading "..path
-			textlabel.BackgroundTransparency = 1
-			textlabel.TextStrokeTransparency = 0
-			textlabel.TextSize = 30
-			textlabel.Font = Enum.Font.SourceSans
-			textlabel.TextColor3 = Color3.new(1, 1, 1)
-			textlabel.Position = UDim2.new(0, 0, 0, -36)
-			textlabel.Parent = kavo["MainGui"]
-			repeat task.wait() until betterisfile(path)
-			textlabel:Remove()
-		end)
-		local req = requestfunc({
-			Url = "https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/"..path:gsub("Nightbed/assets", "assets"),
-			Method = "GET"
-		})
-		writefile(path, req.Body)
-	end
-	return getasset(path) 
+if not betterisfile(path) then
+spawn(function()
+	local textlabel = Instance.new("TextLabel")
+	textlabel.Size = UDim2.new(1, 0, 0, 36)
+	textlabel.Text = "Downloading "..path
+	textlabel.BackgroundTransparency = 1
+	textlabel.TextStrokeTransparency = 0
+	textlabel.TextSize = 30
+	textlabel.Font = Enum.Font.SourceSans
+	textlabel.TextColor3 = Color3.new(1, 1, 1)
+	textlabel.Position = UDim2.new(0, 0, 0, -36)
+	textlabel.Parent = kavo["MainGui"]
+	repeat task.wait() until betterisfile(path)
+	textlabel:Remove()
+	end)
+local req = requestfunc({
+	Url = "https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/"..path:gsub("Nightbed/assets", "assets"),
+	Method = "GET"
+})
+writefile(path, req.Body)
+end
+return getasset(path)
 end
 
-local sha = loadstring(GetURL("Libraries/sha.lua"))()
-shared.shaonline = sha
+local shalib = loadstring(GetURL("Libraries/sha.lua"))()
+shared.shaonline = shalib
+local entity = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/Libraries/entityHandler.lua"))()
+shared.VapeEntity = entity
 if isfolder(customdir:gsub("/", "")) == false then
-	makefolder(customdir:gsub("/", ""))
+makefolder(customdir:gsub("/", ""))
 end
 if isfolder("Nightbed") == false then
-	makefolder("Nightbed")
+makefolder("Nightbed")
 end
 if isfolder("Nightbed/CustomModules") == false then
-	makefolder("Nightbed/CustomModules")
+makefolder("Nightbed/CustomModules")
 end
 if isfolder("Nightbed/Profiles") == false then
-	makefolder("Nightbed/Profiles")
+makefolder("Nightbed/Profiles")
 end
 if isfolder("Nightbed/assets") == false then
-	makefolder("Nightbed/assets")
+makefolder("Nightbed/assets")
 end
 
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/6872274481.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/6872274481.lua"))()
 elseif game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua"))()
 elseif isfile("Nightbed/CustomModules/"..game.PlaceId..".lua") then
-    loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
-else    
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
+loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
 end
