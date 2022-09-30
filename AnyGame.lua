ba6c5cb776bac26bc79e175c612316006860a601
MOONCoreDev <114739237+MOONCoreDev@users.noreply.github.com>
2022-09-30 17:46:48 +0700

    Update AnyGame.lua

--- AnyGame.lua
+++ AnyGame.lua
@@ -1,1 +1,10 @@
+--[[in development everything try working on it lol
+Credit -- infnite Yeild
+-- xylex 7granddadpgn , Moerii , Everyone that i added
+]]
 
+
+local kavo = shared.kavoU
+local sha = shared.shaonline
+local entity = shared.VapeEntity
+local whitelists = shared.inlist
