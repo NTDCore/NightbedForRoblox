c49d282f5502420450c550e88be33380d9b99c92
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-12-03 14:38:03 +0700

    Update GuiLibrary.lua

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -1,1 +1,12 @@
-
+--soon!
+local api = {
+  ["ToggleUI"] = RightShift,
+  ["RainbowUI"] = false,
+  ["Notification"] = true,
+  ["AnimationUI"] = true,
+  ["ObjectsThatCanBeSaved"] = {}
+}
+local uicount = 0
+api["CreateWindow"] = function()
+  uicount += 0.5
+ end
