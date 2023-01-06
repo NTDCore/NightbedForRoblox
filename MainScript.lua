3dee0377046fab54303ae1c3e4b7f606cf756b40
NTDCore <102583640+NTDCore@users.noreply.github.com>
2023-01-06 15:52:36 +0700

    update

--- MainScript.lua
+++ MainScript.lua
@@ -9,6 +9,7 @@ else
 	shared.injected = true
 end
 
+
 function betterfile(path)
 	local suc, res = pcall(function() return readfile(path) end)
 	return suc and res ~= nil 
@@ -19,8 +20,8 @@ if placeId == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/6872274481.lua"))()
 elseif game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..placeid..".lua") then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..placeid..".lua"))()
-elseif betterfile("Nightbed/CustomModules/"..placeid..".lua") then
-    loadstring(betterfile("Nightbed/CustomModules/"..placeid..".lua"))()
+elseif readfile("Nightbed/CustomModules/"..placeid..".lua") then
+    loadstring(readfile("Nightbed/CustomModules/"..placeid..".lua"))()
 else    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
 end
