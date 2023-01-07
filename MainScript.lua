a098f21eda17fe02c162d4e177ed379841136f90
NTDCore <102583640+NTDCore@users.noreply.github.com>
2023-01-07 17:35:56 +0700

    update

--- MainScript.lua
+++ MainScript.lua
@@ -15,13 +15,19 @@ function betterfile(path)
 	return suc and res ~= nil 
 end
 
-local placeid = game.PlaceId
-if placeId == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/MainScript.lua'))()")
+    end
+end)
+
+if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/6872274481.lua"))()
-elseif game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..placeid..".lua") then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..placeid..".lua"))()
-elseif readfile("Nightbed/CustomModules/"..placeid..".lua") then
-    loadstring(readfile("Nightbed/CustomModules/"..placeid..".lua"))()
+elseif game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua"))()
+elseif readfile("Nightbed/CustomModules/"..game.PlaceId..".lua") then
+    loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
 else    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
 end
