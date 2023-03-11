f33f28551153f0250115d831ad8d9e1d6503ecf3
NTDCore <102583640+NTDCore@users.noreply.github.com>
2023-03-11 08:27:49 +0700

    Create NewMainScript.lua

---
+++ NewMainScript.lua
@@ -0,0 +1,5 @@
+if not game.PlaceId == 6872274481 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua", true))()
+else
+  print"fuck i will do later"
+end
