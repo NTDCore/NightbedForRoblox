f33f2bcb3a71ec74c699ef1b0c69ad79155c98ec
Monia <102583640+NTDCore@users.noreply.github.com>
2023-04-21 15:30:07 +0700

    Delete the file at 'NewMainScript.lua'

--- NewMainScript.lua
+++
@@ -1,5 +0,0 @@
-if not game.PlaceId == 6872274481 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua", true))()
-else
-  print"fuck i will do later"
-end
