0fa9234e4568a8d5d82a669942e11395716f2180
Monia <102583640+NTDCore@users.noreply.github.com>
2023-04-25 13:34:21 +0700

    update Libraries/FunctionsHandler.lua

--- Libraries/FunctionsHandler.lua
+++ Libraries/FunctionsHandler.lua
@@ -15,7 +15,7 @@ end
 Functions = {
 	executor = identifyexecutor()
 }
-Functions.RobloxNotification = function(first, second, timewa)
+function Functions.RobloxNotification(first, second, timewa)
 	game.StarterGui:SetCore("SendNotification", {
     Title = first;
     Text = second;
