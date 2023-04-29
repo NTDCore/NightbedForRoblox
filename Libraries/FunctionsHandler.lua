801d9219cc83da0ee335f83e8a93848d0c9b83ac
Monia <102583640+NTDCore@users.noreply.github.com>
2023-04-29 19:59:36 +0700

    stupid

--- Libraries/FunctionsHandler.lua
+++ Libraries/FunctionsHandler.lua
@@ -6,22 +6,23 @@ shared.FuncsConnect = true
 local setidentity = syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or function() end
 local getidentity = syn and syn.get_thread_identity or get_thread_identity or getidentity or getthreadidentity or function() return 0 end
 
-Functions.runcode = function(func)
+do
+	Functions.runcode = function(func)
 		func()
-end
-Functions.runFunction = function(func)
+	end
+	Functions.runFunction = function(func)
 		func()
-end
-Functions = {
-	executor = identifyexecutor()
-}
-function Functions.RobloxNotification(first, second, timewa)
-	game.StarterGui:SetCore("SendNotification", {
-    Title = first;
-    Text = second;
-    Duration = timewa;
-})
-end
+	end
+	Functions = {
+		executor = identifyexecutor()
+	}
+	function Functions.RobloxNotification(first, second, timewa)
+		game.StarterGui:SetCore("SendNotification", {
+    	Title = first;
+    	Text = second;
+  		Duration = timewa;
+		})
+	end
 --[[
 Functions.displayErrorPopup = function(title, text, textbutton, funclist)
 	local oldidentity = getidentity()
@@ -59,10 +60,17 @@ Functions.displayErrorPopup = function(title, text, textbutton, funclist)
 	setidentity(oldidentity)
 end
 --]]
+end
 
 if Functions.executor:find("Arceus") then
-		Functions.RobloxNotification("Detected", "you executor not support for FunctionsHandler game will shutdown in 5s\n Executor : Arceus", 5)
-		wait(5)
-		shared.FuncsLib = false
-		game:Shutdown()
+	Functions.RobloxNotification("Detected", "you executor not support for FunctionsHandler game will shutdown in 5s\n Executor : Arceus", 5)
+	wait(5)
+	shared.FuncsConnect = false
+	game:Shutdown()
 end
+
+if shared.FuncsConnect then
+	print("Functions Has Been Connected!")
+end
+
+return Functions
