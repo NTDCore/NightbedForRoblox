0b3905d7ffde32b5cb4174ad5971cab53afde12f
Monia <102583640+NTDCore@users.noreply.github.com>
2023-05-04 15:19:57 +0700

    update Libraries/mercury.lua

--- Libraries/mercury.lua
+++ Libraries/mercury.lua
@@ -3104,7 +3104,7 @@ function Library:keybind(options)
 
 	local methods = {}
 
-	function methods:Set(keycode)
+	function methods:SetKeyCode(keycode)
 		options.Keybind = keycode
 		keybindDisplay.Text = (options.Keybind and tostring(options.Keybind.Name):upper()) or "?"
 		keybindDisplay:tween{Size = UDim2.fromOffset(keybindDisplay.TextBounds.X + 20, 20), Length = 0.05}
