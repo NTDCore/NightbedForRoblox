dc668acca1a2579f88078c87dc0e53bac821f7e7
Monia <102583640+NTDCore@users.noreply.github.com>
2023-05-27 19:50:37 +0700

    :trollface:

--- MainScript.lua
+++ MainScript.lua
@@ -1,3 +1,4 @@
+-- Thanks For KenzuX for helping me about the fucking fix frfr
 repeat task.wait() until game:IsLoaded()
 local isfile = isfile or function(path)
 	local suc, res = pcall(function() return readfile(path) end)
@@ -7,12 +8,12 @@ end
 local kavo
 kavo = loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/Libraries/kavo.lua", true))()
 shared.kavogui = kavo
+local Sections = {}
+shared.SectionsLoaded = Sections
 
 local entityLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/Libraries/entityHandler.lua", true))()
 local FunctionsLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/Libraries/FunctionsHandler.lua", true))()
 shared.funcslib = FunctionsLibrary
-local Settings = {}
-shared.Settings = Settings
 
 local queueteleport = queue_on_teleport
 
@@ -33,26 +34,47 @@ end
 if isfolder("Nightbed/Profiles") == false then
   makefolder("Nightbed/Profiles")
 end
-
+local AnyGame = [[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua",
+true))()
+]]
 if isfolder("Nightbed/CustomModules") == false then
 	makefolder("Nightbed/CustomModules")
 end
 
 function MainLoaded()
-  if not shared.NightbedLoaded then
-    if game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
-  	  loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua", true))()
-    elseif isfile("Nightbed/CustomModules/"..game.PlaceId..".lua") and game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
-      loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
-    else
-    	loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
+  local customModuleURL = "https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua"
+  local customModuleScript = game:HttpGet(customModuleURL, true)
+  if customModuleScript then
+    local success, error = pcall(function()
+      loadstring(customModuleScript)()
+    end)
+    if not success then
+      --warn("Error loading custom module: " .. tostring(error))
+      --FunctionsLibrary.displayErrorPopup("Error", "CustomModules Not Found\nLaunch As AnyGame\n Error: "
+      .. tostring(error), "OK")
+      loadstring(AnyGame)()
     end
+  else
+    loadstring(AnyGame)()
   end
 end
 
+--[[
+function MainLoaded()
+  if game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
+  	loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua", true))()
+  elseif isfile("Nightbed/CustomModules/"..game.PlaceId..".lua") then
+    loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
+  end
+end
+--]]
+
 if not shared.NightbedLoaded then
   MainLoaded()
-  task.wait(0.2)
+  task.wait(0.1)
   shared.NightbedLoaded = true
 end
 
