48dbbf992a875a4381191e27eee94ed7774065ac
Monia <102583640+NTDCore@users.noreply.github.com>
2023-08-07 09:45:05 +0700

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,1 +1,12 @@
-
+-- nightbee test log
+local nightbedInjected = true
+local nightbedConsole = function(message)
+  print("[NIGHTBED]: "..message)
+end
+task.wait()
+nightbedConsole("Loading...")
+task.wait(1)
+nightbedConsole("Success! Loaded")
+if isfile("Nightbed/Profiles/"..game.PlaceId..".json") or isfile("Nightbed/Profiles/Universal.json") then
+  nightbedConsole("Loaded Config ("..game.PlaceId..".json")
+end
