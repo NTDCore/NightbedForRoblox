0d4f871c8b0e245d869a0ec1eb9818c377437043
Monia <102583640+NTDCore@users.noreply.github.com>
2023-08-08 08:40:47 +0700

    update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,12 +1,30 @@
--- nightbee test log
+-- nightbed test log
 local nightbedInjected = true
+local baseDirectionFile = (shared.NightbedDeveloper and "NightbedDev/" or "Nightbed/")
 local nightbedConsole = function(message)
   print("[NIGHTBED]: "..message)
 end
-task.wait()
-nightbedConsole("Loading...")
-task.wait(1)
-nightbedConsole("Success! Loaded")
-if isfile("Nightbed/Profiles/"..game.PlaceId..".json") or isfile("Nightbed/Profiles/Universal.json") then
-  nightbedConsole("Loaded Config ("..game.PlaceId..".json")
+
+local githubRequest = function(scripturl)
+  return game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/"..scripturl, true)
 end
+
+local NBFolder = function(folder)
+  if isfolder(folder) == false then
+    makefolder(folder)
+  end
+end
+
+if isfolder(baseDirectionFile) == false then
+  makefolder(baseDirectionFile)
+  NBFolder(baseDirectionFile.."/CustomModules")
+  NBFolder(baseDirectionFile.."/Core")
+  NBFolder(baseDirectionFile.."/assets")
+  NBFolder(baseDirectionFile.."/Profiles")
+else
+  task.wait()
+  nightbedConsole("Loading...")
+  task.wait(1)
+  nightbedConsole("Success! Loaded")
+end
+return loadstring(githubRequest("MainScript.lua"))()
