af1f6f187e542f05b9a2c125bcf1b6960af0d6dd
MoniaTablet <114739237+MOONCoreDev@users.noreply.github.com>
2023-09-20 11:10:36 +0700

    update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -40,13 +40,8 @@ function MainLoaded()
   end
 end
 
-MainLoaded()
-
-if not shared.FuncsConnect then
-  task.spawn(function()
-  	repeat
-  	  task.wait()
-  		shared.FuncsConnect = true
-  	until shared.FuncsConnect
-  end)
+if shared.NBStore.Injected then
+	task.spawn(MainLoaded)
+else
+	MainLoaded()
 end
