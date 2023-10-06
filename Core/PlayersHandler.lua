cefe8dfc62741897d32ea0b4323cbc000bea461c
MoniaTablet <114739237+MOONCoreDev@users.noreply.github.com>
2023-10-06 09:17:06 +0700

    update Core/PlayersHandler.lua

--- Core/PlayersHandler.lua
+++ Core/PlayersHandler.lua
@@ -1,6 +1,6 @@
 local players = game:GetService("Players")
+local lplr = players.LocalPlayer
 local PlayersHandler = {
-	
 	["Alive"] = false,
 	["mainchar"] = {
 		["Head"] = {},
@@ -8,3 +8,13 @@ local PlayersHandler = {
 		["RootPart"] = {}
 	}
 }
+
+PlayersHandler.isAlive = function(plr, check)
+	plr = plr or lplr
+	check = check or true
+	if plr and check then
+		PlayersHandler.Alive = check
+		return plr and plr.Character and plr.Character.Parent ~= nil and plr.Character.HumanoidRootPart and plr.Character.Head and plr.Character.Humanoid
+	end
+end
+return PlayersHandler
